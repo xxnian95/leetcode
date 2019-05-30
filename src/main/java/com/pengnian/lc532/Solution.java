2196f38acfedb1ff3876b8a5d83836ca8a9d7c44
Zhang Pengnian <zhangpengnian95@hotmail.com>
2019-05-30 22:16:25 +0800

    532. 数组中的K-diff数对 执行用时 : 433 ms, 在K-diff Pairs in an Array的Java提交中击败了11.26% 的用户 内存消耗 : 49.1 MB, 在K-diff Pairs in an Array的Java提交中击败了34.30% 的用户

--- src/main/java/com/pengnian/lc532/Solution.java
+++ src/main/java/com/pengnian/lc532/Solution.java
@@ -3,6 +3,7 @@ package com.pengnian.lc532;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 
@@ -29,30 +30,47 @@ class Solution {
     //  FIXME   难以处理k=0以及多个重复元素的情况
     private int findPairs(int[] nums, int k) {
 
-        HashSet<Integer> set = new HashSet<>();
+//        HashSet<Integer> set = new HashSet<>();
+//        for (int integer : nums) {
+//            set.add(integer);
+//        }
+//        if (k == 0) {
+//            return nums.length - set.size();
+//        }
+//        HashSet<Integer[]> kDiff = new HashSet<>();
+//        ArrayList<Integer> list = new ArrayList<>(set);
+//        for (int integer : list) {
+//            if (list.contains(integer + k)) {
+//                Integer[] findOne = new Integer[2];
+//                findOne[0] = integer;
+//                findOne[1] = integer + k;
+//                kDiff.add(findOne);
+//            }
+//            if (list.contains(integer - k)) {
+//                Integer[] findOne = new Integer[2];
+//                findOne[0] = integer - k;
+//                findOne[1] = integer;
+//                kDiff.add(findOne);
+//            }
+//        }
+//        return kDiff.size() / 2;
+        if (k < 0)
+            return 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        HashSet<String> set = new HashSet<>();
         for (int integer : nums) {
-            set.add(integer);
+            list.add(integer);
         }
-        if (k == 0) {
-            return nums.length - set.size();
-        }
-        HashSet<Integer[]> kDiff = new HashSet<>();
-        ArrayList<Integer> list = new ArrayList<>(set);
-        for (int integer : list) {
+        for (int integer : nums) {
+            list.remove(list.indexOf(integer));
             if (list.contains(integer + k)) {
-                Integer[] findOne = new Integer[2];
-                findOne[0] = integer;
-                findOne[1] = integer + k;
-                kDiff.add(findOne);
+                String current = Integer.toString(integer) + '-' + k;
+                if (!set.contains(current)){
+                    set.add(current);
+                }
             }
-            if (list.contains(integer - k)) {
-                Integer[] findOne = new Integer[2];
-                findOne[0] = integer - k;
-                findOne[1] = integer;
-                kDiff.add(findOne);
-            }
+            list.add(integer);
         }
-        return kDiff.size() / 2;
-
+        return set.size();
     }
 }
