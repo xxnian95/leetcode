fec8dcb3cd757e4a318c4a6c98cb7efe4989422d
Zhang Pengnian <zhangpengnian95@hotmail.com>
2019-06-26 14:50:35 +0800

    452. 用最少数量的箭引爆气球 执行用时 : 143 ms , 在所有 Java 提交中击败了 5.71% 的用户 内存消耗 : 63.9 MB , 在所有 Java 提交中击败了 34.59% 的用户

--- src/main/java/com/pengnian/lc452/Solution.java
+++ src/main/java/com/pengnian/lc452/Solution.java
@@ -13,15 +13,25 @@ class Solution {
 
     @Test
     void test() {
-        int[][] points = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+        int[][] points = {{10, 16}, {2, 5}, {1, 6}, {7, 12}};
         System.out.println(findMinArrowShots(points));
     }
 
-    //  FIXME
+
     private int findMinArrowShots(int[][] points) {
 
-        Arrays.sort(points, Comparator.comparingInt(o -> o[1]));
+        if (points.length == 0)
+            return 0;
+        Arrays.sort(points, Comparator.comparingInt(o -> o[1]));    //  按照每组第二个元素的大小给数组排序
+        int count = 1, end = points[0][1];
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] <= end) {
+                continue;   //  对于这两个数组，较大的第一个数字小于较小的第二个数字，因此能够用一个飞镖
+            }
+            count++;
+            end = points[i][1]; //  不能再用一个飞镖，因此end换为下一个数组的第二个数字
+        }
 
-        return 0;
+        return count;
     }
 }
