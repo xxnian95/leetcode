a4fe1eaf450e2ea53627577d6ed569997253c7b5
Zhang Pengnian <zhangpengnian95@hotmail.com>
2019-07-02 16:00:56 +0800

    665. 非递减数列 运行过慢

---
+++ src/main/java/com/pengnian/lc665/Solution.java
@@ -0,0 +1,25 @@
+package com.pengnian.lc665;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author Zhang Pengnian
+ * @since 2019-07-02 15:03
+ */
+class Solution {
+
+    @Test
+    void test() {
+        int[] nums = {4, 2, 3,2};
+        System.out.println(checkPossibility(nums));
+    }
+
+    private boolean checkPossibility(int[] nums) {
+
+
+
+    }
+
+}
