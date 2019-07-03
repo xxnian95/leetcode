61b52bfb00b892dae6153518445b650baae3fd9c
Zhang Pengnian <zhangpengnian95@hotmail.com>
2019-07-04 00:02:40 +0800

    665. 非递减数列 执行用时 : 5 ms , 在所有 Java 提交中击败了 18.96% 的用户 内存消耗 : 51 MB , 在所有 Java 提交中击败了 27.21% 的用户

--- src/main/java/com/pengnian/lc665/Solution.java
+++ src/main/java/com/pengnian/lc665/Solution.java
@@ -12,13 +12,26 @@ class Solution {
 
     @Test
     void test() {
-        int[] nums = {4, 2, 3,2};
+        int[] nums = {4, 2, 3, 4};
         System.out.println(checkPossibility(nums));
     }
 
     private boolean checkPossibility(int[] nums) {
 
-
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] >= nums[i - 1])
+                continue;
+            else {
+                count++;
+                if (i - 2 >= 0 && nums[i - 2] > nums[i]) {
+                    nums[i] = nums[i - 1];
+                } else {
+                    nums[i - 1] = nums[i];
+                }
+            }
+        }
+        return count <= 1;
 
     }
 
