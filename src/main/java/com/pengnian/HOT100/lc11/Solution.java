cb960c0207281090b5a9481f1f49a77ceab2ba64
Zhang Pengnian <zhangpengnian95@hotmail.com>
2019-07-09 17:17:58 +0800

    11. 盛最多水的容器 执行用时 : 392 ms , 在所有 Java 提交中击败了 29.55% 的用户 内存消耗 : 45.4 MB , 在所有 Java 提交中击败了 48.07% 的用户

---
+++ src/main/java/com/pengnian/HOT100/lc11/Solution.java
@@ -0,0 +1,34 @@
+package com.pengnian.HOT100.lc11;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Zhang Pengnian
+ * @since 2019-07-09 17:09
+ */
+class Solution {
+
+    @Test
+    void test() {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height));
+    }
+
+    private int maxArea(int[] height) {
+
+        int i = 0;
+        int area = 0;
+        while (i < height.length) {
+            int j = height.length - 1;
+            while (j > i) {
+                int currentArea = (j - i) * Math.min(height[i], height[j]);
+                if (currentArea > area)
+                    area = currentArea;
+                j--;
+            }
+            i++;
+        }
+        return area;
+        
+    }
+}
